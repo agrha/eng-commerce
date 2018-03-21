fd1e08b8f2063d428594fec37d3685612ccdb202
agrha <agrhakartz@gmail.com>
2018-03-22 06:51:13 +0700

    goods

---
+++ server/controllers/goods.js
@@ -0,0 +1,127 @@
+const goodSchema = require('../models/Goods')
+
+class Goods {
+  static viewAll(req,res){
+    goodSchema.find()
+    .populate(categoryId)
+    .then(data => {
+      res.status(200).json({
+        message:'list items:',
+        data
+      })
+    })
+    .catch(err=>{
+      res.status(500).json({message:err})
+    })
+  }
+
+  static create (req,res){
+    let obj = {
+      name:req.body.name,
+      quantity:req.body.quantity,
+      description:req.body.description,
+      price:req.body.price,
+      image:req.file.imageUrl,
+      categoryId:req.body.categoryId
+    }
+    goodSchema.create(obj)
+    .then(data=>{
+      res.status(200).json({
+        message:'good has been created',
+        data
+      })
+    })
+    .catch(err=>{
+      res.status(500).json({
+        message:'something went wrong',
+        err
+      })
+    })
+  }
+
+  static update(req,res){
+    let target = {
+      _id:req.params.id
+    }
+    let obj = {
+      name:req.body.name,
+      quantity:req.body.quantity,
+      description:req.body.description,
+      price:req.body.price,
+      image:req.file.imageUrl,
+      categoryId:req.body.categoryId
+    }
+    goodSchema.findOneAndUpdate(target,obj,{new:true})
+    .then(data =>{
+      res.status(200).json({
+        message:'item has been updated'
+      })
+    })
+    .catch(err=>{
+      res.status(500).json({
+        message:'something went wrong',
+        err
+      })
+    })
+  }
+
+  static findOne (req,res) {
+    let target = {
+      _id:req.params.id
+    }
+    goodSchema.findOne(target)
+    .then(data=>{
+      res.status(200).json({
+        message:'data found',
+        data
+      })
+    })
+    .catch(err=>{
+      res.status(500).json({
+        message:'something went wrong',
+        err
+      })
+    })
+  }
+
+  static findByCategory(req,res){
+    let target = {
+      categoryId:req.params.categoryId
+    }
+    goodSchema.find(target)
+    .populate('categoryId')
+    .then(data=>{
+      res.status(200).json({
+        message:'this is list item in this category',
+        data
+      })
+    })
+    .catch(err=>{
+      res.status(500).json({
+        message:'something went wrong',
+        err
+      })
+    })
+  }
+
+  static delete (req,res){
+    let target = {
+      _id:req.params.id
+    }
+    goodSchema.findOneAndRemove(target)
+    .then(data=>{
+      res.status(200).json({
+        message:'goods successfully deleted',
+        data
+      })
+    })
+    .catch(err=>{
+      res.status(500).json({
+        message:'something went wrong',
+        err
+      })
+    })
+  }
+}
+
+module.exports = Goods
